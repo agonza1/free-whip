2d0b763ec22bd038e23ec1596bcf86b1c5babcf6
alberto gonzalez trastoy <albertogontras@gmail.com>
2021-10-22 17:39:36 -0500

    basics working

--- cli-client/janus.py
+++ cli-client/janus.py
@@ -4,103 +4,121 @@ import logging
 import random
 import string
 import time
+import math
+
+import cv2
+import numpy
+from av import VideoFrame
 
 import aiohttp
 
-from aiortc import RTCPeerConnection, RTCSessionDescription, VideoStreamTrack
-from aiortc.contrib.media import MediaPlayer, MediaRecorder
+from aiortc import RTCPeerConnection, RTCSessionDescription, VideoStreamTrack, RTCIceServer, RTCConfiguration
+from aiortc.contrib.media import MediaPlayer
 
 pcs = set()
 
-def transaction_id():
-    return "".join(random.choice(string.ascii_letters) for x in range(12))
+class FlagVideoStreamTrack(VideoStreamTrack):
+    """
+    A video track that returns an animated flag.
+    """
 
+    def __init__(self):
+        super().__init__()  # don't forget this!
+        self.counter = 0
+        height, width = 480, 640
 
-class JanusPlugin:
-    def __init__(self, session, url):
-        self._queue = asyncio.Queue()
-        self._session = session
-        self._url = url
+        # generate flag
+        data_bgr = numpy.hstack(
+            [
+                self._create_rectangle(
+                    width=213, height=480, color=(255, 0, 0)
+                ),  # blue
+                self._create_rectangle(
+                    width=214, height=480, color=(255, 255, 255)
+                ),  # white
+                self._create_rectangle(width=213, height=480, color=(0, 0, 255)),  # red
+            ]
+        )
 
-    async def send(self, payload):
-        message = {"janus": "message", "transaction": transaction_id()}
-        message.update(payload)
-        async with self._session._http.post(self._url, json=message) as response:
-            data = await response.json()
-            assert data["janus"] == "ack"
+        # shrink and center it
+        M = numpy.float32([[0.5, 0, width / 4], [0, 0.5, height / 4]])
+        data_bgr = cv2.warpAffine(data_bgr, M, (width, height))
 
-        response = await self._queue.get()
-        assert response["transaction"] == message["transaction"]
-        return response
+        # compute animation
+        omega = 2 * math.pi / height
+        id_x = numpy.tile(numpy.array(range(width), dtype=numpy.float32), (height, 1))
+        id_y = numpy.tile(
+            numpy.array(range(height), dtype=numpy.float32), (width, 1)
+        ).transpose()
 
+        self.frames = []
+        for k in range(30):
+            phase = 2 * k * math.pi / 30
+            map_x = id_x + 10 * numpy.cos(omega * id_x + phase)
+            map_y = id_y + 10 * numpy.sin(omega * id_x + phase)
+            self.frames.append(
+                VideoFrame.from_ndarray(
+                    cv2.remap(data_bgr, map_x, map_y, cv2.INTER_LINEAR), format="bgr24"
+                )
+            )
+
+    def _create_rectangle(self, width, height, color):
+        data_bgr = numpy.zeros((height, width, 3), numpy.uint8)
+        data_bgr[:, :] = color
+        return data_bgr    
 
 class JanusSession:
-    def __init__(self, url):
+    def __init__(self, url, room):
         self._http = None
-        self._poll_task = None
-        self._plugins = {}
         self._root_url = url
         self._session_url = None
+        self._session_room = room
+        self._offersdp = None
+        self._answersdp = None
 
-    async def attach(self, plugin_name: str) -> JanusPlugin:
-        message = {
-            "janus": "attach",
-            "plugin": plugin_name,
-            "transaction": transaction_id(),
-        }
-        async with self._http.post(self._session_url, json=message) as response:
-            data = await response.json()
-            assert data["janus"] == "success"
-            plugin_id = data["data"]["id"]
-            plugin = JanusPlugin(self, self._session_url + "/" + str(plugin_id))
-            self._plugins[plugin_id] = plugin
-            return plugin
-
-    async def create(self):
+    async def createEndpoint(self, offer):
         self._http = aiohttp.ClientSession()
-        message = {"janus": "create", "transaction": transaction_id()}
-        async with self._http.post(self._root_url, json=message) as response:
-            data = await response.json()
-            assert data["janus"] == "success"
-            session_id = data["data"]["id"]
-            self._session_url = self._root_url + "/" + str(session_id)
-
-        self._poll_task = asyncio.ensure_future(self._poll())
+        self._offersdp = offer.sdp
+        headers = {'content-type': 'application/sdp', 'Authorization' : 'Bearer verysecret'}
+        async with self._http.post(self._root_url + '/whip/endpoint/1234', headers=headers, data=self._offersdp) as response:
+            print(response)
+            location = response.headers["Location"]
+            assert isinstance(location, str)
+            self._answersdp = await response.text()
+            self._session_url = self._root_url + location
+            print(self._session_url)
+      
+    async def trickle(self, data):
+        self._http = aiohttp.ClientSession()
+        headers = {'content-type': 'application/trickle-ice-sdpfrag', 'Authorization' : 'Bearer verysecret'}
+        async with self._http.patch(self._session_url, headers=headers, data=data) as response:
+            print(response)
+            data = await response.text()
 
     async def destroy(self):
-        if self._poll_task:
-            self._poll_task.cancel()
-            self._poll_task = None
-
         if self._session_url:
-            message = {"janus": "destroy", "transaction": transaction_id()}
-            async with self._http.post(self._session_url, json=message) as response:
-                data = await response.json()
-                assert data["janus"] == "success"
+            headers = {'Authorization' : 'Bearer verysecret'}
+            async with self._http.delete(self._session_url, headers=headers) as response:
+                print(response)
+                assert response.ok == True
             self._session_url = None
 
         if self._http:
             await self._http.close()
             self._http = None
 
-    async def _poll(self):
-        while True:
-            params = {"maxev": 1, "rid": int(time.time() * 1000)}
-            async with self._http.get(self._session_url, params=params) as response:
-                data = await response.json()
-                if data["janus"] == "event":
-                    plugin = self._plugins.get(data["sender"], None)
-                    if plugin:
-                        await plugin._queue.put(data)
-                    else:
-                        print(data)
 
+async def publish(session, player):
+    """
+    Live stream video to the room.
+    """
+    pc = RTCPeerConnection(configuration=RTCConfiguration(
+        iceServers=[RTCIceServer(
+            urls=["stun:stun.l.google.com:19302", 
+            "turn:host.docker.internal:3478"], 
+            username="turnuser", 
+            credential="turnpassword")]))
 
-async def publish(plugin, player):
-    """
-    Send video to the room.
-    """
-    pc = RTCPeerConnection()
     pcs.add(pc)
 
     # configure media
@@ -112,107 +130,34 @@ async def publish(plugin, player):
     if player and player.video:
         pc.addTrack(player.video)
     else:
-        pc.addTrack(VideoStreamTrack())
+        pc.addTrack(FlagVideoStreamTrack())
+        #pc.addTrack(VideoStreamTrack())
 
     # send offer
-    await pc.setLocalDescription(await pc.createOffer())
-    request = {"request": "configure"}
-    request.update(media)
-    response = await plugin.send(
-        {
-            "body": request,
-            "jsep": {
-                "sdp": pc.localDescription.sdp,
-                "trickle": True,
-                "type": pc.localDescription.type,
-            },
-        }
-    )
+    offer = await pc.createOffer()
+    await pc.setLocalDescription(offer)
 
+    #Create WHIP endpoint with the SDP offer
+    await session.createEndpoint(offer)
+    print(session) 
     # apply answer
     await pc.setRemoteDescription(
         RTCSessionDescription(
-            sdp=response["jsep"]["sdp"], type=response["jsep"]["type"]
+            sdp=session._answersdp, type="answer"
         )
     )
 
-
-async def subscribe(session, room, feed, recorder):
-    pc = RTCPeerConnection()
-    pcs.add(pc)
-
-    @pc.on("track")
-    async def on_track(track):
-        print("Track %s received" % track.kind)
-        if track.kind == "video":
-            recorder.addTrack(track)
-        if track.kind == "audio":
-            recorder.addTrack(track)
-
-    # subscribe
-    plugin = await session.attach("janus.plugin.videoroom")
-    response = await plugin.send(
-        {"body": {"request": "join", "ptype": "subscriber", "room": room, "feed": feed}}
-    )
-
-    # apply offer
-    await pc.setRemoteDescription(
-        RTCSessionDescription(
-            sdp=response["jsep"]["sdp"], type=response["jsep"]["type"]
-        )
-    )
-
-    # send answer
-    await pc.setLocalDescription(await pc.createAnswer())
-    response = await plugin.send(
-        {
-            "body": {"request": "start"},
-            "jsep": {
-                "sdp": pc.localDescription.sdp,
-                "trickle": True,
-                "type": pc.localDescription.type,
-            },
-        }
-    )
-    await recorder.start()
-
-
-async def run(player, recorder, room, session):
-    await session.create()
-
-    # join video room
-    plugin = await session.attach("janus.plugin.videoroom")
-    response = await plugin.send(
-        {
-            "body": {
-                "display": "aiortc",
-                "ptype": "publisher",
-                "request": "join",
-                "room": room,
-            }
-        }
-    )
-    publishers = response["plugindata"]["data"]["publishers"]
-    for publisher in publishers:
-        print("id: %(id)s, display: %(display)s" % publisher)
-
+async def run(player, session):
     # send video
-    await publish(plugin=plugin, player=player)
-
-    # receive video
-    if recorder is not None and publishers:
-        await subscribe(
-            session=session, room=room, feed=publishers[0]["id"], recorder=recorder
-        )
-
-    # exchange media for 10 minutes
-    print("Exchanging media")
-    await asyncio.sleep(600)
+    await publish(session=session, player=player)
+    # exchange media for 1 minute
+    print("Exchanging media...")
+    await asyncio.sleep(60)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Janus")
-    parser.add_argument("url", help="Janus root URL, e.g. http://localhost:8088/janus")
+    parser.add_argument("url", help=" WHIP root URL, e.g. http://localhost:7080/whip")
     parser.add_argument(
         "--room",
         type=int,
@@ -220,7 +165,6 @@ if __name__ == "__main__":
         help="The video room ID to join (default: 1234).",
     ),
     parser.add_argument("--play-from", help="Read the media from a file and sent it."),
-    parser.add_argument("--record-to", help="Write received media to a file."),
     parser.add_argument("--verbose", "-v", action="count")
     args = parser.parse_args()
 
@@ -228,7 +172,7 @@ if __name__ == "__main__":
         logging.basicConfig(level=logging.DEBUG)
 
     # create signaling and peer connection
-    session = JanusSession(args.url)
+    session = JanusSession(args.url, args.room)
 
     # create media source
     if args.play_from:
@@ -236,24 +180,15 @@ if __name__ == "__main__":
     else:
         player = None
 
-    # create media sink
-    if args.record_to:
-        recorder = MediaRecorder(args.record_to)
-    else:
-        recorder = None
-
     loop = asyncio.get_event_loop()
     try:
         loop.run_until_complete(
-            run(player=player, recorder=recorder, room=args.room, session=session)
+            run(player=player, session=session)
         )
     except KeyboardInterrupt:
         pass
     finally:
-        if recorder is not None:
-            loop.run_until_complete(recorder.stop())
         loop.run_until_complete(session.destroy())
-
         # close peer connections
         coros = [pc.close() for pc in pcs]
         loop.run_until_complete(asyncio.gather(*coros))
