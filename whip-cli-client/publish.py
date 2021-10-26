6409b322cb872d5affba9db6b13bba1467830fb1
alberto gonzalez trastoy <albertogontras@gmail.com>
2021-10-26 18:53:02 -0500

    simplify

--- whip-cli-client/publish.py
+++ whip-cli-client/publish.py
@@ -1,11 +1,6 @@
 import argparse
 import asyncio
 import logging
-import math
-
-import cv2
-import numpy
-from av import VideoFrame
 
 import aiohttp
 
@@ -13,56 +8,6 @@ from aiortc import RTCPeerConnection, RTCSessionDescription, VideoStreamTrack, R
 from aiortc.contrib.media import MediaPlayer
 
 pcs = set()
-
-class FlagVideoStreamTrack(VideoStreamTrack):
-    """
-    A video track that returns an animated flag.
-    """
-
-    def __init__(self):
-        super().__init__()  # don't forget this!
-        self.counter = 0
-        height, width = 480, 640
-
-        # generate flag
-        data_bgr = numpy.hstack(
-            [
-                self._create_rectangle(
-                    width=213, height=480, color=(255, 0, 0)
-                ),  # blue
-                self._create_rectangle(
-                    width=214, height=480, color=(255, 255, 255)
-                ),  # white
-                self._create_rectangle(width=213, height=480, color=(0, 0, 255)),  # red
-            ]
-        )
-
-        # shrink and center it
-        M = numpy.float32([[0.5, 0, width / 4], [0, 0.5, height / 4]])
-        data_bgr = cv2.warpAffine(data_bgr, M, (width, height))
-
-        # compute animation
-        omega = 2 * math.pi / height
-        id_x = numpy.tile(numpy.array(range(width), dtype=numpy.float32), (height, 1))
-        id_y = numpy.tile(
-            numpy.array(range(height), dtype=numpy.float32), (width, 1)
-        ).transpose()
-
-        self.frames = []
-        for k in range(30):
-            phase = 2 * k * math.pi / 30
-            map_x = id_x + 10 * numpy.cos(omega * id_x + phase)
-            map_y = id_y + 10 * numpy.sin(omega * id_x + phase)
-            self.frames.append(
-                VideoFrame.from_ndarray(
-                    cv2.remap(data_bgr, map_x, map_y, cv2.INTER_LINEAR), format="bgr24"
-                )
-            )
-
-    def _create_rectangle(self, width, height, color):
-        data_bgr = numpy.zeros((height, width, 3), numpy.uint8)
-        data_bgr[:, :] = color
-        return data_bgr    
 
 class WhipSession:
     def __init__(self, url, token, room):
@@ -78,7 +23,7 @@ class WhipSession:
         self._http = aiohttp.ClientSession()
         self._offersdp = offer.sdp
         headers = {'content-type': 'application/sdp', 'Authorization' : 'Bearer ' + self._token}
-        async with self._http.post(self._root_url + '/whip/endpoint/1234', headers=headers, data=self._offersdp) as response:
+        async with self._http.post(self._root_url + '/whip/endpoint/' + str(self._session_room), headers=headers, data=self._offersdp) as response:
             print(response)
             location = response.headers["Location"]
             assert isinstance(location, str)
@@ -136,8 +81,7 @@ async def publish(session, player):
     if player and player.video:
         pc.addTrack(player.video)
     else:
-        pc.addTrack(FlagVideoStreamTrack())
-        # pc.addTrack(VideoStreamTrack())
+        pc.addTrack(VideoStreamTrack())
 
     # send offer
     offer = await pc.createOffer()
